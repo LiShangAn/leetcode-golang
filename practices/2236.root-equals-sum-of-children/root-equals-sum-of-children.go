ef53a061c226f7355eacb2a451ee5c80cadaa241
LiShangAn <gunhogantz@gmail.com>
2022-06-18 05:28:41 +0800

    #2236. Root Equals Sum of Children

---
+++ practices/2236.root-equals-sum-of-children/root-equals-sum-of-children.go
@@ -0,0 +1,24 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func checkTree(root *TreeNode) bool {
+
+	if root.Left.Val+root.Right.Val != root.Val {
+		return false
+	}
+	return true
+}
