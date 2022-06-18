919d9433fd41cd8b5db728940cf36a02148e3bb0
LiShangAn <gunhogantz@gmail.com>
2022-06-18 15:20:49 +0800

    #.1603. Design Parking System

---
+++ practices/1603.design-parking-system/design-parking-system.go
@@ -0,0 +1,23 @@
+package leetcode
+
+type ParkingSystem struct {
+	left [3]int
+}
+
+func Constructor(big int, medium int, small int) ParkingSystem {
+	return ParkingSystem{[3]int{big, medium, small}}
+}
+
+func (this *ParkingSystem) AddCar(carType int) bool {
+	if this.left[carType-1] > 0 {
+		this.left[carType-1]--
+		return true
+	}
+	return false
+}
+
+/**
+* Your ParkingSystem object will be instantiated and called as such:
+* obj := Constructor(big, medium, small);
+* param_1 := obj.AddCar(carType);
+ */
