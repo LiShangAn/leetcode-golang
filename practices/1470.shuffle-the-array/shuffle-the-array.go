4880cd2132248075f6b175c27783e3c80b5a1685
LiShangAn <gunhogantz@gmail.com>
2022-06-18 08:55:49 +0800

    #1470. Shuffle The Array

---
+++ practices/1470.shuffle-the-array/shuffle-the-array.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func shuffle(nums []int, n int) []int {
+
+	var result []int
+
+	for i := 0; i < n; i++ {
+		result = append(result, nums[i])
+		result = append(result, nums[i+n])
+	}
+
+	return result
+}
