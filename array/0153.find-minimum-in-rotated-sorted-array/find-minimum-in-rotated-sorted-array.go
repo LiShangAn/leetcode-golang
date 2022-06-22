4ed6bc74023e07e087240b13f281e092c2330c21
LiShangAn <gunhogantz@gmail.com>
2022-06-22 23:06:37 +0800

    update solution

--- array/0153.find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ array/0153.find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,6 +1,30 @@
 package leetcode
 
-func findMin(nums []int) int {
+func findMin1(nums []int) int {
+
+	low := 0
+
+	high := len(nums) - 1
+
+	for low < high {
+		median := (low + high) / 2
+
+		if nums[low] < nums[high] {
+			return nums[low]
+		}
+
+		if nums[median] > nums[high] {
+			low = median + 1
+		} else {
+			high = median
+		}
+	}
+
+	return nums[low]
+
+}
+
+func findMin2(nums []int) int {
 
 	min := nums[0]
 
